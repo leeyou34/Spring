eac42393424bac3517e174e1e91251e35ba19600
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-16 16:40:02 +0900

    first commit of Annotation (page 97)

---
+++ BoardWebAnnotation/src/main/java/polymorphism/LgTV.java
@@ -0,0 +1,28 @@
+package polymorphism;
+
+import org.springframework.stereotype.Component;
+
+@Component("tv")
+public class LgTV implements TV {
+
+	public LgTV() {
+		System.out.println("===> LgTV 객체 생성");
+	}
+	
+	public void powerOn() {
+		System.out.println("LgTV---전원 켠다.");
+	}
+	
+	public void powerOff() {
+		System.out.println("LgTV---전원 끈다.");
+	}
+	
+	public void volumeUp() {
+		System.out.println("LgTV---소리 올린다.");
+	}
+	
+	public void volumeDown() {
+		System.out.println("LgTV---소리 낮춘다.");
+	}
+	
+}
