538c34cc1c343bd0aa86dc52ebc9080f1647e1b5
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-20 17:06:51 +0900

    End of JoinPoint와 바인드 변수

--- BoardWebAOP/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
+++ BoardWebAOP/src/main/java/com/springbook/biz/common/AfterThrowingAdvice.java
@@ -1,7 +1,21 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.JoinPoint;
+import org.springframework.format.annotation.NumberFormat;
+
 public class AfterThrowingAdvice {
-	public void exceptionLog() {
-		System.out.println("[예외 처리] 비지니스 로직 수행 중 예외 발생");
+	public void exceptionLog(JoinPoint jp, Exception exceptObj) {
+		String method = jp.getSignature().getDeclaringTypeName();
+		System.out.println(method + "() 메소드 수행 중 예외 발생");
+		
+		if(exceptObj instanceof IllegalArgumentException) {
+			System.out.println("부적합한 값이 입력되었습니다.");
+		} else if(exceptObj instanceof NumberFormat) {
+			System.out.println("숫자 형식의 값이 아닙니다.");
+		} else if(exceptObj instanceof Exception) {
+			System.out.println("문제가 발생했습니다.");
+		}
+		
+		// System.out.println("[예외 처리] " + method + "() 메소드 수행 중 발생된 예외 메세지 : " + exceptObj.getMessage());
 	}
 }
--- BoardWebAOP/src/main/java/com/springbook/biz/common/AroundAdvice.java
+++ BoardWebAOP/src/main/java/com/springbook/biz/common/AroundAdvice.java
@@ -1,12 +1,27 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.springframework.util.StopWatch;
 
 public class AroundAdvice {
-	public Object AroundLog(ProceedingJoinPoint pjp) throws Throwable{
-		System.out.println("[BEFORE]: 비지니스 메소드 수행 전에 처리할 내용...");
-		Object returnObj= pjp.proceed();
-		System.out.println("[AFTER]: 비지니스 메소드 수행 후에 처리할 내용...");
-		return returnObj;
+	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable{
+		//System.out.println("[BEFORE]: 비지니스 메소드 수행 전에 처리할 내용...");
+		//Object returnObj= pjp.proceed();
+		 
+		
+		Object method= pjp.getSignature().getName();
+		
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		
+		Object obj= pjp.proceed();
+		
+		stopWatch.stop();
+
+		System.out.println(method + "() 메소드 수행에 걸린 시간 : " + stopWatch.getTotalTimeMillis() + "(ms)초");
+		
+		//System.out.println("[AFTER]: 비지니스 메소드 수행 후에 처리할 내용...");
+		//return returnObj;
+		return obj;
 	}
 }
