59eb3f30222e63d775e8f0f9f412e79bfae6c369
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-20 12:21:48 +0900

    log4j advice 사용하도록 수정 완료

--- BoardWebAOP/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWebAOP/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -7,41 +7,48 @@ import org.springframework.stereotype.Service;
 
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
-import com.springbook.biz.common.LogAdvice;
+import com.springbook.biz.common.Log4Advice;
 
 @Service("boardService")
 public class BoardServiceImpl implements BoardService {
 
 	@Autowired
 	private BoardDAO boardDAO;
-	private LogAdvice log;//로그용 AOP 적용
+	//private LogAdvice log;//로그용 AOP 적용
+	private Log4Advice log;
 	
 	public BoardServiceImpl() { // 로그 AOP용 생성자 코드 적용
-		log = new LogAdvice(); // 로그 메소드 적용
+	//	log = new LogAdvice(); // 로그 메소드 적용
+		log = new Log4Advice();
 	}
 	
 	public void insertBoard(BoardVO vo) {
-		log.printLog();
+		//log.printLog();
+		log.printLogging();
 		boardDAO.insertBoard(vo);
 	}
 
 	public void updateBoard(BoardVO vo) {
-		log.printLog();
+		//log.printLog();
+		log.printLogging();
 		boardDAO.updateBoard(vo);
 	}
 
 	public void deleteBoard(BoardVO vo) {
-		log.printLog();
+		//log.printLog();
+		log.printLogging();
 		boardDAO.deleteBoard(vo);
 	}
 
 	public BoardVO getBoard(BoardVO vo) {
-		log.printLog();
+		//log.printLog();
+		log.printLogging();
 		return boardDAO.getBoard(vo);
 	}
 
 	public List<BoardVO> getBoardList(BoardVO vo) {
-		log.printLog();
+		//log.printLog();
+		log.printLogging();
 		return boardDAO.getBoardList(vo);
 	}
 
