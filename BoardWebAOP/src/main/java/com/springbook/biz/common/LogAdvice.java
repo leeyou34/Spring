916bf316d6a95bbace5037faf00d1931f704cf3b
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-20 17:28:35 +0900

    Pointcut과 Before 어노테이션 적용

--- BoardWebAOP/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ BoardWebAOP/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,5 +1,6 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +10,14 @@ public class LogAdvice {
 	//	System.out.println("[공통 로그] 비지니스 로직 수행 전 동작");
 	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
 	public void allPointcut() {}
+	
+	//public void printLog(JoinPoint jp) {
+	
+	@Before("allPointcut()")
+	public void printLog() {
+		System.out.println("[공통 로그] 비지니스 로직 수행 전 동작");
+	}	
+	
 	
 	@Pointcut("execution(* com.springbook.biz..*Impl.get*(..))")
 	public void getPointcut() {}
