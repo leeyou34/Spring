4f47ebde1308ae559bd4a92a9ab7ea18186e3754
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-21 13:21:02 +0900

    After advice class has been modified

--- BoardWebAOP/src/main/java/com/springbook/biz/common/AroundAdvice.java
+++ BoardWebAOP/src/main/java/com/springbook/biz/common/AroundAdvice.java
@@ -1,15 +1,25 @@
 package com.springbook.biz.common;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
 import org.springframework.util.StopWatch;
 
+@Service
+@Aspect
 public class AroundAdvice {
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut() {}
+	
+	@Around("allPointcut()")
 	public Object aroundLog(ProceedingJoinPoint pjp) throws Throwable{
 		//System.out.println("[BEFORE]: 비지니스 메소드 수행 전에 처리할 내용...");
 		//Object returnObj= pjp.proceed();
 		 
-		
-		Object method= pjp.getSignature().getName();
+		String method= pjp.getSignature().getName();
+		//Object method= pjp.getSignature().getName();
 		
 		StopWatch stopWatch = new StopWatch();
 		stopWatch.start();
