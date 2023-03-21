f33e8177c661260c51fbcc471ae590066540044f
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-21 12:52:21 +0900

    5.2.1. Before 어드바이스의 어드바이스 동작 시점 세팅

--- BoardWebAOP/src/main/java/com/springbook/biz/common/LogAdvice.java
+++ BoardWebAOP/src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,19 +1,21 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Service;
 
 @Service
+@Aspect // Aspect = Pointcut + Advice
 public class LogAdvice {
 	//public void printLog(JoinPoint jp) {
 	//	System.out.println("[공통 로그] 비지니스 로직 수행 전 동작");
-	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))") // 포인트 컷
 	public void allPointcut() {}
 	
 	//public void printLog(JoinPoint jp) {
 	
-	@Before("allPointcut()")
+	@Before("allPointcut()") // 어드바이스
 	public void printLog() {
 		System.out.println("[공통 로그] 비지니스 로직 수행 전 동작");
 	}	
