4058926f6366cd73cc357e968fd1f0fc1aec1f18
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-21 14:08:05 +0900

    BeforeAdvice

--- BoardWebAOP/src/main/java/com/springbook/biz/common/BeforeAdvice.java
+++ BoardWebAOP/src/main/java/com/springbook/biz/common/BeforeAdvice.java
@@ -14,12 +14,20 @@ public class BeforeAdvice {
 		
 	}
 	
-	@Before("allPointcut()")
+/*	@Before("allPointcut()")
 	public void beforeLog(JoinPoint jp) {
 		String method = jp.getSignature().getName();
 		Object[] args = jp.getArgs();
 		
 		//System.out.println("[사전 처리] 비지니스 로직 수행 전 동작");
 		System.out.println("[사전 처리] " + method + "() 메소드 ARGS 정보: " + args[0].toString());
+	} */
+	
+	@Before("PointcutCommon.allPointcut()")
+	public void beforeLog(JoinPoint jp) {
+		String method = jp.getSignature().getDeclaringTypeName();
+		Object[] args = jp.getArgs();
+		
+		System.out.println("[사전 처리] " + method +"() 메소드 ARGS 정보 : " + args[0].toString());
 	}
 }
