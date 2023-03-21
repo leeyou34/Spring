91b1538b06ea55e58416c3637583517567ceafe5
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-21 15:30:55 +0900

    fix the 주석

--- BoardWebJDBC/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
+++ BoardWebJDBC/src/main/java/com/springbook/biz/board/impl/BoardDAO.java
@@ -21,9 +21,9 @@ public class BoardDAO {
 	// SQL 명령어들 ====> postgreSQL 버젼으로 고치도록 고려할 것
 	private final String BOARD_INSERT= "insert into board(seq, title, writer, content) values(select nvl(max(seq), 0)+1 from board),?,?,?)";
 	private final String BOARD_UPDATE= "update board set title=?, content=? where seq=?";
-	//private final String BOARD_DELETE= "delete board where seq=?";
-	//private final String BOARD_GET= "select * from board where seq=?";
-	//private final String BOARD_LIST= "select * from board order by seq desc";
+	private final String BOARD_DELETE= "delete board where seq=?";
+	private final String BOARD_GET= "select * from board where seq=?";
+	private final String BOARD_LIST= "select * from board order by seq desc";
 	
 	
 	//CRUD 기능의 메소드 구현
@@ -66,7 +66,7 @@ public class BoardDAO {
 	
 	
 	// 글 삭제
-/*	
+	
 		public void deleteBoard(BoardVO vo) {
 			System.out.println("====> JDBC로 updateBoard() 기능 처리");
 			try {
@@ -132,5 +132,5 @@ public class BoardDAO {
 				JDBCUtil.close(stmt,  conn);
 			}
 			return boardList;
-		}*/
+		}
 }
