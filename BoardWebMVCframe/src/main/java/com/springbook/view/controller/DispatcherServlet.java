2c004e68511a16a72d20612e927cf482903fc630
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-24 16:26:27 +0900

    MVC 프레임 워크 DIspatcher Servlet 수정

--- BoardWebMVCframe/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ BoardWebMVCframe/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -19,7 +19,17 @@ import com.springbook.biz.user.impl.UserDAO;
  */
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+    
+	private HandlerMapping handlerMapping;
+	
+	private ViewResolver viewResolver;
+	
+	public void init() throws ServletException {
+		handlerMapping = new HandlerMapping();
+		viewResolver = new ViewResolver();
+		viewResolver.setPrefix("./");
+		viewResolver.setSuffix(".jsp");
+	}
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -53,6 +63,23 @@ public class DispatcherServlet extends HttpServlet {
 		String path = uri.substring(uri.lastIndexOf("/"));
 		System.out.println(path);
 		
+		// 2. HandlerMapping을 통해 path에 해당하는 Controller를 검색한다.
+		Controller ctrl = handlerMapping.getController(path);
+		
+		// 3. 검색된 Controller를 실행한다.
+		String viewName = ctrl.handleRequest(request, response);
+		
+		// 4. viewResolver를 통해 viewName에 해당하는 화면을 검색한다.
+		String view = null;
+		if(!viewName.contains(".do")) {
+			view = viewResolver.getView(viewName);
+		} else {
+			view = viewName;
+		}
+		
+		// 5. 검색된 화면으로 이동한다.
+		response.sendRedirect(view);
+/*		
 		// 2. 클라이언트의 요청 path에 따라 적절히 분기처리 한다.
 		if(path.equals("/login.do")) {
 			System.out.println("로그인 처리");
@@ -172,6 +199,6 @@ public class DispatcherServlet extends HttpServlet {
 			HttpSession session = request.getSession();
 			session.setAttribute("boardList", boardList);
 			response.sendRedirect("getBoardList.jsp");
-		}
+		}*/
 	}		
 }
