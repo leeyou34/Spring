06bee713130c3d910fc7973d1ae3ebbcd7883c45
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-24 11:47:07 +0900

    글 등록 기능 구현

--- BoardWebModel2/src/main/java/com/springbook/view/controller/DispatcherServlet.java
+++ BoardWebModel2/src/main/java/com/springbook/view/controller/DispatcherServlet.java
@@ -80,6 +80,25 @@ public class DispatcherServlet extends HttpServlet {
 			System.out.println("로그아웃 처리");
 		} else if(path.equals("/insertBoard.do")) {
 			System.out.println("글 등록 처리");
+			
+			// 1. 사용자 입력 정보 추출
+			// request.setCharacterEncoding("EUC-KR");
+			String title = request.getParameter("title");
+			String writer = request.getParameter("writer");
+			String content = request.getParameter("content");
+			
+			// 2. DB 연동 처리
+			BoardVO vo = new BoardVO();
+			vo.setTitle(title);
+			vo.setWriter(writer);
+			vo.setContent(content);
+			
+			BoardDAO boardDAO = new BoardDAO();
+			boardDAO.insertBoard(vo);
+			
+			//3. 화면 네비게이션
+			response.sendRedirect("getBoardList.do");
+			
 		} else if(path.equals("/updateBoard.do")) {
 			System.out.println("글 수정 처리"); 		
 		} else if(path.equals("/deleteBoard.do")) {
