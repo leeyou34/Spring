63b1813fac93a203d6cd7ac6522ea3a978de4e6a
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-25 20:18:47 +0900

    MVC 프레임 워크 개발 중 "글 등록 구현" 업로드

--- BoardWebMVCframe/src/main/java/com/springbook/view/controller/HandlerMapping.java
+++ BoardWebMVCframe/src/main/java/com/springbook/view/controller/HandlerMapping.java
@@ -3,6 +3,8 @@ package com.springbook.view.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.springbook.view.board.GetBoardController;
+import com.springbook.view.board.GetBoardListController;
 import com.springbook.view.user.LoginController;
 
 public class HandlerMapping {
@@ -11,6 +13,8 @@ public class HandlerMapping {
 	public HandlerMapping() {
 		mappings = new HashMap<String, Controller>();
 		mappings.put("/login.do", new LoginController());
+		mappings.put("/getBoardList.do", new GetBoardListController());
+		mappings.put("/getBoard.do",  new GetBoardController());
 	}
 	
 	public Controller getController(String path) {
