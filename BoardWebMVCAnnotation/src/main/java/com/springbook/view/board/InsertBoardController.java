bbc078ba3997f332db52e5aad516f6da54b113f5
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-27 16:58:03 +0900

    어노테이션 기반 MVC 개발

---
+++ BoardWebMVCAnnotation/src/main/java/com/springbook/view/board/InsertBoardController.java
@@ -0,0 +1,40 @@
+package com.springbook.view.board;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.servlet.mvc.Controller;
+
+import com.springbook.biz.board.BoardVO;
+import com.springbook.biz.board.impl.BoardDAO;
+
+@Controller
+public class InsertBoardController /*implements Controller */{
+	//@Override
+	//public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public void insertBoard(HttpServletRequest request) {
+		
+	System.out.println("글 등록 처리");
+		
+		// 1. 사용자 입력 정보 추출
+		// request.setCharacterEncoding("EUC-KR");
+		String title = request.getParameter("title");
+		String writer = request.getParameter("writer");
+		String content = request.getParameter("content");
+		
+		// 2. DB 연동 처리
+		BoardVO vo = new BoardVO();
+		vo.setTitle(title);
+		vo.setWriter(writer);
+		vo.setContent(content);
+		
+		BoardDAO boardDAO = new BoardDAO();
+		boardDAO.insertBoard(vo);
+		
+		// 3. 화면 네비게이션
+		//return "getBoardList.do";
+		//ModelAndView mav = new ModelAndView();
+		//mav.setViewName("getBoardList.do");
+		//return mav;
+	}
+
+}
