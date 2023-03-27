05a33c3416c1d19e40117720cf001fa2d8828bea
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-27 14:08:38 +0900

    글 상세 조회 기능 구현

--- BoardWebMVCapply/src/main/java/com/springbook/view/board/GetBoardController.java
+++ BoardWebMVCapply/src/main/java/com/springbook/view/board/GetBoardController.java
@@ -4,13 +4,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.ModelAndView;
+
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 import com.springbook.view.controller.Controller;
 
 public class GetBoardController implements Controller{
 	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println(" 글 상세 조회 처리");
 		
 		// 1. 검색할 게시글 번호 추출
@@ -24,8 +26,12 @@ public class GetBoardController implements Controller{
 		BoardVO board = boardDAO.getBoard(vo);
 		
 		// 3. 검색 결과를 세션에 저장하고 상세 화면을 리턴
-		HttpSession session = request.getSession();
-		session.setAttribute("board", board);
-		return "getBoard";
+		//HttpSession session = request.getSession();
+		//session.setAttribute("board", board);
+		//return "getBoard";
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("board", board);
+		mav.setViewName("getBoard.jsp");
 	}
 }
--- BoardWebMVCapply/src/main/java/com/springbook/view/board/GetBoardListController.java
+++ BoardWebMVCapply/src/main/java/com/springbook/view/board/GetBoardListController.java
@@ -6,10 +6,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
-import com.springbook.view.controller.Controller;
+
 
 public class GetBoardListController implements Controller{
 	@Override
