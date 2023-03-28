ed04e7af24e7b7bfdade1ba4c53255214838936f
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-28 17:25:23 +0900

    Model Attributes 사용하기 완료

--- BoardWebMVCapply/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardWebMVCapply/src/main/java/com/springbook/view/board/BoardController.java
@@ -1,7 +1,11 @@
 package com.springbook.view.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -42,7 +46,7 @@ public class BoardController {
 	}
 	
 	// 글 목록 검색
-	@RequestMapping("/getBoardList.do")
+/*	@RequestMapping("/getBoardList.do")
 	public String getBoardList(@RequestParam(value="searchCondition", defaultValue="TITLE", required=false) String condition 
 								   , @RequestParam(value="searchKeyword", defaultValue="", required=false) String keyword
 								   , BoardDAO boardDAO, Model model) {
@@ -54,5 +58,22 @@ public class BoardController {
 		//mav.addObject("boardList", boardDAO.getBoardList(vo)); // Model 정보 저장
 		//mav.setViewName("getBoardList.jsp"); // view 정보 저장
 		//return mav;
+	}
+*/	
+	// 검색 조건 목록 설정
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap() {
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목", "TITLE");
+		conditionMap.put("내용", "CONTENT");
+		return conditionMap;
+	}
+	
+	// 글 목록 검색
+	@RequestMapping("/getBoardList.do")
+	public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {
+		// model 정보 저장
+		model.addAttribute("boardList", boardDAO.getBoardList(vo));
+		return "getBoardList.jsp"; // view 이름 리턴
 	}
 }
--- BoardWebMVCapply/src/main/webapp/WEB-INF/views/getBoardList.jsp
+++ BoardWebMVCapply/src/main/webapp/WEB-INF/views/getBoardList.jsp
@@ -32,8 +32,11 @@
 			<tr>
 				<td align="right">
 					<select name="searchCondition">
-						<option value="TITLE">제목
-						<option value="CONTENT">내용
+					<!-- 	<option value="TITLE">제목
+						<option value="CONTENT">내용  -->
+						<c:forEach items="${conditionMap }" var="option">
+							<option value="${option.value }">${option.key }
+						</c:forEach>
 					</select>
 					<input name="searchKeyword" type="text"/>
 					<input name="submit" value="검색"/>
