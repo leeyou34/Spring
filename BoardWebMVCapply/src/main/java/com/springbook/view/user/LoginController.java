701a746a05430a2765d6b3e44aa8cc6ab3ab8d09
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-28 16:47:11 +0900

    servlet API 사용

--- BoardWebMVCapply/src/main/java/com/springbook/view/user/LoginController.java
+++ BoardWebMVCapply/src/main/java/com/springbook/view/user/LoginController.java
@@ -1,5 +1,7 @@
 package com.springbook.view.user;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -60,9 +62,17 @@ public class LoginController {
 	}
 	
 	@RequestMapping(value="/login.do", method=RequestMethod.POST)
-	public String login(UserVO vo, UserDAO userDAO) {
+	public String login(UserVO vo, UserDAO userDAO, HttpSession session) {
 		System.out.println("로그인 인증 처리...");
-		if(userDAO.getUser(vo) != null) return "getBoardList.do";
+		
+		UserVO user = userDAO.getUser(vo);
+		
+		if(user != null) {
+			session.setAttribute("userName",  user.getName());
+			return "getBoardList.do";
+		}
 		else return "login.jsp";
+		//if(userDAO.getUser(vo) != null) return "getBoardList.do";
+		//else return "login.jsp";
 	}
 }
--- BoardWebMVCapply/src/main/webapp/WEB-INF/views/getBoardList.jsp
+++ BoardWebMVCapply/src/main/webapp/WEB-INF/views/getBoardList.jsp
@@ -25,7 +25,7 @@
 <body>
 	<center>
 		<h1>글 목록</h1>
-			<h3>테스트님 환영합니다... <a href="logout_proc.jsp">logout.do</a></h3>
+			<h3>${userName}님! 환영합니다... <a href="logout_proc.jsp">logout.do</a></h3>
 		<!-- 검색 시작 -->
 		<form action="getBoardList.jsp" method="post">
 		<table border="1" cellpadding="0" cellspacing="0" width="700">
