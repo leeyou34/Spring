7176a12d6752aaa4c22f9579078fa21df0428d04
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-29 14:40:02 +0900

    글 상세 조회 코드 수정

--- BoardWebMVCapply/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardWebMVCapply/src/main/java/com/springbook/view/board/BoardController.java
@@ -7,13 +7,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
+@SessionAttributes("board")
 public class BoardController {
 	// 글 등록
 	@RequestMapping("/insertBoard.do")
@@ -25,7 +26,13 @@ public class BoardController {
 	
 	// 글 수정
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(BoardVO vo, BoardDAO boardDAO) {
+	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
+		System.out.println("번호 : " + vo.getSeq());
+		System.out.println("제목 : " + vo.getTitle());
+		System.out.println("작성자 : " + vo.getWriter());
+		System.out.println("내용 : " + vo.getContent());
+		System.out.println("등록일 : " + vo.getRegDate());
+		System.out.println("조회수 : " + vo.getCnt());
 		boardDAO.updateBoard(vo);
 		return "getBoardList.do";
 	}
@@ -38,11 +45,16 @@ public class BoardController {
 	}
 	
 	// 글 상세 조회
-	@RequestMapping("/getBoard.do")
+/*	@RequestMapping("/getBoard.do")
 	public ModelAndView getBoard(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
 		mav.addObject("board", boardDAO.getBoard(vo)); // Model 정보 저장
 		mav.setViewName("getBoard.jsp");
 		return mav;
+*/
+	@RequestMapping("/getBoard.do")
+	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
+		model.addAttribute("board", boardDAO.getBoard(vo)); // Model 정보 저장
+		return "getBoard.jsp"; // View 이름 리턴
 	}
 	
 	// 글 목록 검색
