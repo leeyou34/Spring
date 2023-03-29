8cb6635ea17d27e8ab4157f628de7281f3b02034
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-29 15:28:44 +0900

    프리젠테이션 레이어와 비지니스 레이어 통합

--- BoardWebMVCapply/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
+++ BoardWebMVCapply/src/main/java/com/springbook/biz/board/impl/BoardServiceImpl.java
@@ -29,7 +29,7 @@ public class BoardServiceImpl implements BoardService {
 //			throw new IllegalArgumentException("0번 글은 등록할 수 없습니다.");
 //		}
 		boardDAO.insertBoard(vo);
-		boardDAO.insertBoard(vo);
+		//boardDAO.insertBoard(vo);
 	}
 
 	public void updateBoard(BoardVO vo) {
