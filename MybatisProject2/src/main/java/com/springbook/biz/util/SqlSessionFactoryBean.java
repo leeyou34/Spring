0084b1dd955d3c44d322d3f034471b8fb757a699
yh.thomas.lee <yh.thomas.lee@gmail.com>
2023-03-31 12:22:31 +0900

    SqlSession 객체 생성

--- MybatisProject2/src/main/java/com/springbook/biz/util/SqlSessionFactoryBean.java
+++ MybatisProject2/src/main/java/com/springbook/biz/util/SqlSessionFactoryBean.java
@@ -6,12 +6,31 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-
+/*
 public class SqlSessionFactoryBean {
 	private static SqlSessionFactory sessionFactory = null;
 	static {
 		try {
 			if ( sessionFactory == null) {
+				Reader reader = Resources.getResourceAsReader("sql-map-config.xml");
+				sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static SqlSession getSqlSessionInstance() {
+		return sessionFactory.openSession();
+	}
+}
+*/
+
+public class SqlSessionFactoryBean {
+	private static SqlSessionFactory sessionFactory = null;
+	static {
+		try {
+			if (sessionFactory == null) {
 				Reader reader = Resources.getResourceAsReader("sql-map-config.xml");
 				sessionFactory = new SqlSessionFactoryBuilder().build(reader);
 			}
